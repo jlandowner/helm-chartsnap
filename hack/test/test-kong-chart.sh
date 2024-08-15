989b4bb444653d561fe50a9a87a18ae353cd9009
jlandowner <jlandowner8@gmail.com>
2024-08-16 08:10:11 +0900

    Fix to exec helm repo add for dependencies

--- hack/test/test-kong-chart.sh
+++ hack/test/test-kong-chart.sh
@@ -3,10 +3,20 @@
 # Update submodule to the latest commit
 git submodule update --init --recursive --remote --merge
 
+[[ `which yq` ]] && YQ=yq || \
+    (
+        wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O ./yq
+        chmod +x ./yq
+        YQ=./yq
+    )
+
 # Loop through each directory in charts/charts and run helm dependency update
 for dir in charts/charts/*; do
     if [ -d "$dir" ]; then
         cd "$dir"
+        $YQ e '.dependencies[] | .name + " " + .repository' Chart.lock | while read line; do 
+            helm repo add $line --force-update
+        done
         helm dependency build
         cd -
     fi
